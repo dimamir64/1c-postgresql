32512d5adeaf219a2c7fc7b9c68de6b7f05eabac
Anton Oslyak <anton@sirius.if.ua>
2020-07-14 22:20:11 +0300

    Forgotten commit

--- run.sh
+++ run.sh
@@ -2,16 +2,13 @@
 
 PGDATA="/var/1C/postgresql-10/data"
 
-# sudo [ -d "$PGDATA" ] || sudo mkdir -p "$PGDATA" && sudo chown $USER "$PGDATA"
 docker volume create 1c-data
 
-docker container run -dit --network=host --hostname=1c-postgresql --name=1c-postgresql \
+docker container run -d --network=host --hostname=1c-postgresql --name=1c-postgresql \
                      --restart=always                       \
-                     -e "POSTGRES_USER=postgres"            \
-                     -e "POSTGRES_PASSWORD=password"        \
+                     -e "POSTGRES_USER=sirius"            \
+                     -e "POSTGRES_PASSWORD=pYd8ie9I"        \
                      -e "PGDATA=$PGDATA" \
-                     -e "LANG=ru_RU.UTF-8"                  \
-                     -e "LANGUAGE=ru"                       \
                      --mount source=1c-data,target="$PGDATA" \
                      oslyak/1c-postgresql
 
